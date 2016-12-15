740ddaf1a64524c5ab6323dfced79b6ff3b52e27
Peter Birsinger <pbirsinger@gmail.com>
2016-12-15 14:03:24 -0500

    add hot reloading baby

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -3,14 +3,28 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
 
-config.devtool = "#eval-source-map"
+var ip = 'localhost'
 
-config.plugins = config.plugins.concat([
-  new BundleTracker({filename: './webpack-stats-local.json'}),
-])
+config.devtool = "#eval-source-map"
 
 config.module.loaders.push(
   { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
 )
+
+config.entry = {
+  App1: [
+    'webpack-dev-server/client?http://' + ip + ':3000',
+    'webpack/hot/only-dev-server',
+    './reactjs/App1',
+  ],
+}
+
+config.output.publicPath = 'http://' + ip + ':3000' + '/assets/bundles/'
+
+config.plugins = config.plugins.concat([
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new BundleTracker({filename: './webpack-stats-local.json'}),
+])
 
 module.exports = config
