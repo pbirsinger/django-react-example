f3af45ca6ce9b007f9f30dab422d3b4fb682aa0d
Peter Birsinger <pbirsinger@gmail.com>
2016-12-16 21:30:18 -0500

    add redux + styling; up to step 8

---
+++ reactjs/actions/counterActions.js
@@ -0,0 +1,5 @@
+
+export const INCREASE = "INCREASE"
+export function increaseCounter() {
+    return { type: INCREASE }
+}
--- reactjs/containers/App1Container.jsx
+++ reactjs/containers/App1Container.jsx
@@ -1,14 +1,49 @@
 import React from "react"
+import Radium from "radium"
 
+import { connect } from "react-redux"
+import {bindActionCreators} from 'redux';
+
+import * as counterActions from "../actions/counterActions"
 import Headline from "../components/Headline"
 
-export default class App1Container extends React.Component {
+
+const styles = {
+  button: {
+    cursor: "pointer",
+  },
+  counter: {
+    color: "blue",
+    fontSize: "20px",
+  }
+}
+
+
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(counterActions, dispatch);
+}
+
+@connect(state => ({
+  counters: state.counters,
+}), mapDispatchToProps)
+@Radium
+export default class SampleAppContainer extends React.Component {
+  handleClick() {
+    console.log("handdling");
+    this.props.increaseCounter();
+  }
+
   render() {
+    let {counters} = this.props
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
             <Headline>Sample App!</Headline>
+            <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
+             <p style={[styles.counter]}> {counters.clicks}</p>
+            <p>{process.env.BASE_API_URL}</p>
           </div>
         </div>
       </div>
